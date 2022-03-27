bratkartoff/ModernCombat

commit 889d905b3c006c3bc607ee68c39add5472822403

Spawn at a different flag if there are enemies nearby

ModernCombat.c4d/Goals.c4d/Occupation.c4d/FlagPost.c4d/Script.c
@@ -75,6 +75,11 @@ public func SetStartFlagForTeam(int teamnumber)
   Capture(teamnumber, true);
 }
 
+public func IsStartFlagForTeam(int teamnumber)
+{
+  return startflagforteam == teamnumber;
+}
+
 // todo: colors
 public func InitCapturableArray()
 {
@@ -88,7 +93,7 @@ public func IsCapturableBy(int teamnumber)
   if (!IsFrontlines())
     return true;
   // start flags are always recapturable
-  if (teamnumber == startflagforteam)
+  if (IsStartFlagForTeam())
     return true;
   // set by Frontlines goal object via SetCapturableBy
   return capturableby[teamnumber];
@@ -103,6 +108,11 @@ public func CountCapturableBy()
     if(IsCapturableBy(GetTeamByIndex(i)))
       teams++;
   return teams;
+}
+
+public func IsBacklineFlag()
+{
+  return CountCapturableBy() == 1 && IsFullyCaptured();
 }
 
 public func SetCapturableBy(int teamnumber, bool capturable)
@@ -170,23 +180,7 @@ public func GetScoreboardPercentColor()
 
 public func AddSpawnPoint(int iX, int iY, string szFunction)
 {
-  var i = GetLength(spawnpoints);
-  spawnpoints[i] = CreateArray();
-  spawnpoints[i][0] = AbsX(iX);
-  spawnpoints[i][1] = AbsY(iY);
-  spawnpoints[i][2] = szFunction;
-}
-
-public func GetSpawnPoint(int &iX, int &iY, string &szFunction, int iPlr)
-{
-  if(!GetLength(spawnpoints))
-  {
-    iY = -30;
-    return;
-  }
-
-  var iX, iY, szFunction;
-  szFunction = GetBestSpawnpoint(spawnpoints, iPlr, iX, iY)[2];
+  spawnpoints[] = [iX, iY, szFunction];
 }
 
 public func ResetSpawnPoint()
@@ -578,14 +572,6 @@ public func MoveFlagpost(int iX, int iY, string szName, int iRange, bool fNeutra
 
   //Reichweite setzen
   if(iRange) range = iRange;
-
-  //Spawnpunkte anpassen
-  var curX = GetX(), curY = GetY();
-  for(var i = 0; i < GetLength(spawnpoints); i++)
-  {
-    spawnpoints[i][0] -= iX - curX;
-    spawnpoints[i][1] -= iY - curY;
-  }
 
   //Reichweitenanzeige vorhanden: Entfernen
   if(captureradiusmarker)

ModernCombat.c4d/Goals.c4d/Frontlines.c4d/Script.c
@@ -249,8 +249,7 @@ private func UpdateScoreboard()
   for(var flag in GetFlags())
   {
     var namecolor = flag->GetNameColor();
-    var teams = flag->CountCapturableBy();
-    if (!teams || teams == 1 && flag->IsFullyCaptured())
+    if (!flag->CountCapturableBy() || flag->IsBacklineFlag())
       namecolor = flag->Desaturate(namecolor);
 
     SetScoreboardData(row, GOCC_FlagColumn, Format("<c %x>%s</c>", namecolor, GetName(flag)), row);
@@ -763,6 +762,31 @@ public func OnClassSelection(object pClonk, int iClass)
 
   CreateGOCCSpawner(pClonk, iClass);
 }
+
+public func GetSpawnPoint(object selectedFlag, int &iX, int &iY, string &szFunction, int iPlr)
+{
+  var spawnpoints = [];
+  var team = GetPlayerTeam(iPlr);
+  if (
+    selectedFlag->IsStartFlagForTeam(team) ||
+    selectedFlag->IsBacklineFlag()
+  )
+    spawnpoints = selectedFlag.spawnpoints;
+  else
+  {
+    // frontline flag: also consider other flags in group and neighboring groups
+    var group = GetGroup(selectedFlag);
+    var neighbors = GetNeighbors(group);
+    neighbors[] = group;
+    for (var flaggroup in neighbors)
+      for (var flag in flaggroup)
+        if (flag->IsSpawnableForTeam(team))
+          spawnpoints ..= flag.spawnpoints;
+  }
+
+  szFunction = global->GetBestSpawnpoint(spawnpoints, iPlr, iX, iY)[2];
+}
+
 
 public func DoFlag(int iTeam, int iPlr)
 {

ModernCombat.c4d/Goals.c4d/Occupation.c4d/FlagChooser.c4d/Script.c
@@ -241,10 +241,11 @@ public func SelectFlagpole(object flag)
 
   SetPlrViewRange(Min(200, oldvisrange), crew);//Nicht mehr als maximal 200px sehen.
 
-  var X, Y, szFunc;
-  flag->GetSpawnPoint(X, Y, szFunc, GetOwner(crew));
+  var X = 0, Y = 0, szFunc;
+  FindGOCC()->GetSpawnPoint(flag, X, Y, szFunc, GetOwner(crew));
+  //flag->GetSpawnPoint(X, Y, szFunc, GetOwner(crew));
 
-  SetPosition(GetX(flag) + X, GetY(flag) + Y);
+  SetPosition(X, Y);
 
   if(szFunc) szFunction = szFunc;
 

ModernCombat.c4d/Goals.c4d/Occupation.c4d/Script.c
@@ -848,6 +848,11 @@ public func OnClassSelection(object pClonk, int iClass)
   CreateGOCCSpawner(pClonk, iClass);
 }
 
+public func GetSpawnPoint(object selectedFlag, int &iX, int &iY, string &szFunction, int iPlr)
+{
+  szFunction = global->GetBestSpawnpoint(selectedFlag.spawnpoints, iPlr, iX, iY)[2];
+}
+
 public func DoFlag(int iTeam, int iPlr)
 {
   var pCrew = GetCrew(iPlr);

ModernCombat.c4d/Libraries.c4d/DefaultScript.c4d/Script.c
@@ -237,6 +237,7 @@ global func GetBestSpawnpoint(array aSpawnpoints, int iPlr, int &x, int &y)
     spawn_grading[i] = [spawn, 0];
     for(var obj in FindObjects(Find_Distance(SPAWNSYS_CheckRange, spawn[0], spawn[1]), Find_Or(Find_OCF(OCF_CrewMember), Find_And(Find_Hostile(iPlr), Find_Func("IsSpawnTrap")))))
     {
+      Log("found object %v, spawn", obj, spawn);
       if(GetOCF(obj) & OCF_CrewMember)
       {
         if(Hostile(iPlr, GetOwner(obj)))
@@ -255,6 +256,7 @@ global func GetBestSpawnpoint(array aSpawnpoints, int iPlr, int &x, int &y)
 
   var highest = 0x80000000;
   var chosen_spawns = [];
+  Log("spawn_grading %v", spawn_grading);
   for(var spawn_data in spawn_grading)
   {
     if(spawn_data[1] > highest)
@@ -266,6 +268,7 @@ global func GetBestSpawnpoint(array aSpawnpoints, int iPlr, int &x, int &y)
       chosen_spawns[GetLength(chosen_spawns)] = spawn_data[0];
   }
 
+  Log("chosen_spawns %v", chosen_spawns);
   var spawnpoint = chosen_spawns[Random(GetLength(chosen_spawns))];
   x = spawnpoint[0];
   y = spawnpoint[1];

commit 1855cd1e80525714fc67de9bd0419893b176a2f8

Fix logic error introduced in 889d905

ModernCombat.c4d/Goals.c4d/Occupation.c4d/FlagPost.c4d/Script.c
@@ -93,7 +93,7 @@ public func IsCapturableBy(int teamnumber)
   if (!IsFrontlines())
     return true;
   // start flags are always recapturable
-  if (IsStartFlagForTeam())
+  if (IsStartFlagForTeam(teamnumber))
     return true;
   // set by Frontlines goal object via SetCapturableBy
   return capturableby[teamnumber];